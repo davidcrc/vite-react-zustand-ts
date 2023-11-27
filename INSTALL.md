21336d1930a7a18cb2db877b131d35be49a20b00
davidcrc <david_drcr@hotmail.com>
2023-11-27 15:05:40 -0500

    feat: bears zustand

---
+++ INSTALL.md
@@ -0,0 +1,9 @@
+# Setup
+
+- the basic dashboard is in: basic-dashborad branch
+
+# Zustand
+
+```bash
+npm i zustand
+```
--- src/pages/01-basic/BearPage.tsx
+++ src/pages/01-basic/BearPage.tsx
@@ -1,4 +1,6 @@
+import { useShallow } from "zustand/react/shallow";
 import { WhiteCard } from "../../components";
+import { useBearStore } from "../../stores";
 
 export const BearPage = () => {
   return (
@@ -8,36 +10,100 @@ export const BearPage = () => {
       <hr />
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
-        <WhiteCard centered>
-          <h2>Osos Negros</h2>
+        <BlackBears />
 
-          <div className="flex flex-col md:flex-row">
-            <button> +1</button>
-            <span className="text-3xl mx-2 lg:mx-10"> 0 </span>
-            <button>-1</button>
-          </div>
-        </WhiteCard>
+        <PolarBears />
 
-        <WhiteCard centered>
-          <h2>Osos Polares</h2>
+        <PandaBears />
 
-          <div className="flex flex-col md:flex-row">
-            <button> +1</button>
-            <span className="text-3xl mx-2 lg:mx-10"> 0 </span>
-            <button>-1</button>
-          </div>
-        </WhiteCard>
-
-        <WhiteCard centered>
-          <h2>Osos Pandas</h2>
-
-          <div className="flex flex-col md:flex-row">
-            <button> +1</button>
-            <span className="text-3xl mx-2 lg:mx-10"> 0 </span>
-            <button>-1</button>
-          </div>
-        </WhiteCard>
+        <BearDisplay />
       </div>
     </>
+  );
+};
+
+export const BlackBears = () => {
+  // const { blackBears, increaseBlackBears } = useBearStore( state => state );
+  const increaseBlackBears = useBearStore((state) => state.increaseBlackBears);
+  const blackBears = useBearStore((state) => state.blackBears);
+
+  return (
+    <WhiteCard centered>
+      <h2>Osos Negros</h2>
+
+      <div className="flex flex-col md:flex-row">
+        <button onClick={() => increaseBlackBears(1)}> +1</button>
+        <span className="text-3xl mx-2 lg:mx-10"> {blackBears} </span>
+        <button onClick={() => increaseBlackBears(-1)}>-1</button>
+      </div>
+    </WhiteCard>
+  );
+};
+
+export const PolarBears = () => {
+  // const { polarBears, increasePolarBears } = useBearStore( state => state );
+  // const { polarBears, increasePolarBears } = useBearStore( useShallow( state => state ));
+  const increasePolarBears = useBearStore((state) => state.increasePolarBears);
+  const polarBears = useBearStore((state) => state.polarBears);
+
+  return (
+    <WhiteCard centered>
+      <h2>Osos Polares</h2>
+
+      <div className="flex flex-col md:flex-row">
+        <button onClick={() => increasePolarBears(1)}> +1</button>
+        <span className="text-3xl mx-2 lg:mx-10"> {polarBears} </span>
+        <button onClick={() => increasePolarBears(-1)}>-1</button>
+      </div>
+    </WhiteCard>
+  );
+};
+
+export const PandaBears = () => {
+  // const { pandaBears, increasePandaBears } = useBearStore( state => state );
+  // const increasePandaBears = useBearStore( state => state.increasePandaBears );
+  // const pandaBears = useBearStore( state => state.pandaBears );
+
+  //! Principalmente el shallow es para comparar objectos
+  const { pandaBears, increasePandaBears } = useBearStore(
+    useShallow((state) => ({
+      pandaBears: state.pandaBears,
+      increasePandaBears: state.increasePandaBears,
+    }))
+  );
+
+  return (
+    <WhiteCard centered>
+      <h2>Osos Panda</h2>
+
+      <div className="flex flex-col md:flex-row">
+        <button onClick={() => increasePandaBears(1)}> +1</button>
+        <span className="text-3xl mx-2 lg:mx-10"> {pandaBears} </span>
+        <button onClick={() => increasePandaBears(-1)}>-1</button>
+      </div>
+    </WhiteCard>
+  );
+};
+
+export const BearDisplay = () => {
+  // const bears = useBearStore( state => state.bears );
+  const bears = useBearStore(useShallow((state) => state.bears)); //useShallow, se usa para que el estado bears q estamos tomando aqui no rerenderize , si no ha cambiado
+  const doNothing = useBearStore((state) => state.doNothing);
+  const addBear = useBearStore((state) => state.addBear);
+  const clearBears = useBearStore((state) => state.clearBears);
+
+  return (
+    <WhiteCard>
+      <h1>Osos</h1>
+      <button onClick={doNothing}>No hace nada</button>
+      <button onClick={addBear} className="mt-2">
+        Agregar
+      </button>
+      <button onClick={clearBears} className="mt-2">
+        Borrar
+      </button>
+
+      <pre>{JSON.stringify(bears, null, 2)}</pre>
+    </WhiteCard>
   );
 };
